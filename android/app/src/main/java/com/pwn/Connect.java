0e4770400594dd23f2c93e770d8ac28d771b702c
Dr-D12345 <derekjonp@gmail.com>
2016-12-03 12:39:16 -0500

    fixed multiple errors

--- android/app/src/main/java/com/pwn/Connect.java
+++ android/app/src/main/java/com/pwn/Connect.java
@@ -2,10 +2,7 @@ package com.pwn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.jcraft.jsch.*;
-import java.awt.*;
-import javax.swing.*;
 import java.io.*;
 
 
--- android/app/src/main/java/com/pwn/MainActivity.java
+++ android/app/src/main/java/com/pwn/MainActivity.java
@@ -2,10 +2,6 @@ package com.pwn;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.jcraft.jsch.*;
-import java.awt.*;
-import javax.swing.*;
-import java.io.*;
 
 public class MainActivity extends AppCompatActivity {
 
