5fbccdb28fea4665435fa109b85dd54278c2f4ab
Dr-D12345 <derekjonp@gmail.com>
2016-12-03 12:08:34 -0500

    Connect 0.1

--- android/app/src/main/java/com/pwn/Connect.java
+++ android/app/src/main/java/com/pwn/Connect.java
@@ -15,10 +15,10 @@ public class Connect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
-        JSch jsch=new JSch();
-        String user="root@10.55.176.112";
+        JSch jsch = new JSch();
+        String user = "root@10.55.176.112";
 
-        String host="127.0.0.1";
+        String host = "127.0.0.1";
         UserInfo ui = new UserInfo() {
             @Override
             public String getPassphrase() {
@@ -52,25 +52,54 @@ public class Connect extends AppCompatActivity {
         };
 
 
-        try{
-            Session session=jsch.getSession(host,user,22);
+        try {
+            Session session = jsch.getSession(host, user, 22);
             session.setUserInfo(ui);
             session.connect();
-            Channel channel=session.openChannel("exec");
+            Channel channel = session.openChannel("exec");
 
             channel.setInputStream(null);
 
             String command = "dir";
-            ((ChannelExec)channel).setCommand(command);
+            ((ChannelExec) channel).setCommand(command);
 
-            ((ChannelExec)channel).setErrStream(System.err);
-            channel.connect();
+            ((ChannelExec) channel).setErrStream(System.err);
+            try {
+                InputStream in = channel.getInputStream();
+                channel.connect();
+                byte[] tmp = new byte[1024];
+                while (true) {
+                    while (in.available() > 0) {
+                        int i = in.read(tmp, 0, 1024);
+                        if (i < 0) break;
+                        System.out.print(new String(tmp, 0, i));
+                    }
+                    if (channel.isClosed()) {
+                        if (in.available() > 0) continue;
+                        System.out.println("exit-status: " + channel.getExitStatus());
+                        break;
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception ee) {
+                    }
+                }
 
+                channel.disconnect();
 
+                session.disconnect();
+
+            } catch (Exception e) {
+
+                System.out.println(e);
+
+            }
 
 
         }catch (JSchException je){
-        System.out.println("ERROR ERROR "+ je);
+
+
+            System.out.println("Erorr " + je);
         }
 
 
